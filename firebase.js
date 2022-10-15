d4a10846e9ce7cec9c3f78ed35fcf76fb43805f7
Tejoyasaswi-17 <saisankar1702@gmail.com>
2022-10-15 17:37:01 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtp_j3VyzlK8ZS1qf1_2MUMmkafI8G4W4",
+    authDomain: "clone-f227e.firebaseapp.com",
+    projectId: "clone-f227e",
+    storageBucket: "clone-f227e.appspot.com",
+    messagingSenderId: "809276360663",
+    appId: "1:809276360663:web:2a281b3795f429a4472d92",
+    measurementId: "G-6T4437D71Y"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
